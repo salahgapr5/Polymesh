995145a0e6b125f7eaa6b2f522e31e65dc55555b
Mudit Gupta <guptamudit@ymail.com>
2020-08-20 14:16:03 +0530

    use substrate wasm builder

--- pallets/runtime/develop/build.rs
+++ pallets/runtime/develop/build.rs
@@ -3,7 +3,10 @@ use wasm_builder_runner::WasmBuilder;
 fn main() {
     WasmBuilder::new()
         .with_current_project()
-        .with_wasm_builder_from_path("../../../utils/wasm-builder")
+        .with_wasm_builder_from_git(
+            "https://github.com/paritytech/substrate.git",
+            "b161460bf73e4cc73e29b8e01ab0323bb3d95e84",
+        )
         .export_heap_base()
         .import_memory()
         .build()
